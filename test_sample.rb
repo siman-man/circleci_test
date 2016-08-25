7a3762fdcfd9a7f9ce8e3bc9746651064938a3a2
siman-man <k128585@ie.u-ryukyu.ac.jp>
2016-08-25 23:50:50 +0900

    clone

--- test_sample.rb
+++ test_sample.rb
@@ -1,7 +1,16 @@
 require 'minitest/autorun'
+require 'rugged'
 
 class TestSample < Minitest::Test
   def test_hoge
     assert_equal 'Hello', 'Hello'
+  end
+
+  def test_clone
+    url = 'https://github.com/siman-man/ruby-sandbox.git'
+    path = File.join(Dir.pwd, 'repos/sandbox')
+    Rugged::Repository.clone_at(url, path) unless Dir.exist?(path)
+
+    assert Dir.exist?(path)
   end
 end
